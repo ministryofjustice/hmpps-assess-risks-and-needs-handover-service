b198b0d6a471ac2f75b76649cd8df347ba2e79b4
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-05-20 11:38:46 +0100

    hmpps-handover-init

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppshandoverservice/integration/NotFoundTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppshandoverservice/integration/NotFoundTest.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename.integration
+package uk.gov.justice.digital.hmpps.hmppshandoverservice.integration
 
 import org.junit.jupiter.api.Test
 
