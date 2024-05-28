144a5d7c19ca9c39f7eb31e851721d3d60d6e6fc
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-05-28 10:01:37 +0100

    Allow clients support multiple oauth2 redirect uris

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/config/ClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/config/ClientConfiguration.kt
@@ -23,19 +23,23 @@ class ClientConfiguration(
       .refreshTokenTimeToLive(Duration.ofDays(2))
       .build()
 
-    val registeredClients = clientsProperties.clients.map { (clientId, clientProperties) ->
-      RegisteredClient.withId(clientId)
+    val registeredClients = appConfiguration.clients.map { (clientId, clientProperties) ->
+      val clientBuilder = RegisteredClient.withId(clientId)
         .clientId(clientId)
         .clientSecret("{noop}${clientProperties.secret}")
         .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
         .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
         .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-        .redirectUri(clientProperties.oauthRedirectUri)
         .scope(OidcScopes.OPENID)
         .scope(OidcScopes.PROFILE)
         .tokenSettings(tokenSettings)
         .clientSettings(ClientSettings.builder().build())
-        .build()
+
+      clientProperties.oauthRedirectUris.forEach { uri ->
+        clientBuilder.redirectUri(uri)
+      }
+
+      clientBuilder.build()
     }
 
     return InMemoryRegisteredClientRepository(registeredClients)
