3acb0ef1541c9c26d94af51ed32f611ac3364367
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-07-02 08:20:42 +0100

    Standardise handover request, enable further support for Sentence Plan (#27)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/handover/request/CreateHandoverLinkRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/handover/request/CreateHandoverLinkRequest.kt
@@ -4,16 +4,19 @@ import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.context.entity.HandoverPrincipal
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.context.entity.SubjectDetails
 
-data class HandoverOasysRequest(
+data class CreateHandoverLinkRequest(
   @Schema(description = "Details of the practitioner/principal making the handover request")
   val user: HandoverPrincipal,
 
   @Schema(description = "Details of the PoP/subject of the handover request")
   val subjectDetails: SubjectDetails,
 
+  @Schema(description = "Assessment PK used for matching OASys records to assessment UUID/plan UUID")
   val oasysAssessmentPk: String,
 
-  val assessmentUUID: String?,
-
+  @Schema(description = "Version of the assessment")
   val assessmentVersion: Long?,
+
+  @Schema(description = "Version of the sentence plan")
+  val planVersion: Long?,
 )
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/handover/HandoverServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/handover/HandoverServiceTest.kt
@@ -6,16 +6,22 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.config.AppConfiguration
+import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.context.entity.AssessmentContext
+import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.context.entity.HandoverContext
+import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.context.entity.SentencePlanContext
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.context.service.HandoverContextService
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.handover.entity.HandoverToken
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.handover.entity.TokenStatus
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.handover.repository.HandoverTokenRepository
+import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.handover.request.CreateHandoverLinkRequest
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.handover.service.HandoverService
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.handover.service.UseHandoverLinkResult
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.testUtils.TestUtils
-import java.util.Optional
+import java.util.*
 import kotlin.test.assertContains
 
 class HandoverServiceTest {
@@ -23,60 +29,135 @@ class HandoverServiceTest {
   private lateinit var handoverService: HandoverService
   private val handoverTokenRepository: HandoverTokenRepository = mockk()
   private val handoverContextService: HandoverContextService = mockk()
-  private val appConfiguration: AppConfiguration = mockk()
-  val handoverSessionId = "testSessionId"
+  private val appConfiguration = mockk<AppConfiguration>(relaxed = true)
+  private var handoverSessionId = "testSessionId"
 
   @BeforeEach
   fun setUp() {
     handoverService = HandoverService(handoverTokenRepository, handoverContextService, appConfiguration)
+    handoverSessionId = UUID.randomUUID().toString()
   }
 
-  @Test
-  fun `create handover should save the given handover request and return the link `() {
-    val handoverRequest = TestUtils.createHandoverRequest()
-    val handoverToken = HandoverToken(
-      handoverSessionId = handoverSessionId,
-      principal = handoverRequest.principal,
-    )
-    val handoverContext = TestUtils.createHandoverContext(handoverSessionId)
-    every { handoverContextService.saveContext(handoverSessionId, any()) } returns handoverContext
-    every { handoverTokenRepository.save(any()) } returns handoverToken
-    every { appConfiguration.self.externalUrl } returns "/handover_url"
-    every { appConfiguration.self.endpoints } returns TestUtils.createEndPoint()
-    val result = handoverService.createHandover(handoverRequest, handoverSessionId)
+  @Nested
+  @DisplayName("createHandover")
+  inner class CreateHandover {
+    private lateinit var handoverRequest: CreateHandoverLinkRequest
+    private lateinit var handoverToken: HandoverToken
+    private lateinit var handoverContext: HandoverContext
 
-    assertEquals(handoverSessionId, result.handoverSessionId)
-    assertContains(result.handoverLink, "/handover_url/handover_endpoint/")
+    @BeforeEach
+    fun setUp() {
+      handoverRequest = TestUtils.createHandoverRequest()
+      handoverToken = HandoverToken(
+        handoverSessionId = handoverSessionId,
+        principal = handoverRequest.user,
+      )
+      handoverContext = HandoverContext(
+        handoverSessionId = handoverSessionId,
+        principal = handoverRequest.user,
+        subject = handoverRequest.subjectDetails,
+        assessmentContext = AssessmentContext(
+          oasysAssessmentPk = handoverRequest.oasysAssessmentPk,
+          assessmentVersion = handoverRequest.assessmentVersion,
+        ),
+        sentencePlanContext = SentencePlanContext(
+          oasysAssessmentPk = handoverRequest.oasysAssessmentPk,
+          planVersion = handoverRequest.planVersion,
+        ),
+      )
+    }
 
-    verify { handoverContextService.saveContext(handoverSessionId, any()) }
-    verify { handoverTokenRepository.save(any()) }
+    @Test
+    fun `should save the handover context with correct properties`() {
+      every { handoverContextService.saveContext(any()) } returns handoverContext
+      every { handoverTokenRepository.save(any()) } returns handoverToken
+
+      handoverService.createHandover(handoverRequest, handoverSessionId)
+
+      verify {
+        handoverContextService.saveContext(
+          withArg {
+            assertEquals(handoverSessionId, it.handoverSessionId)
+            assertEquals(handoverContext.subject, it.subject)
+            assertEquals(handoverContext.principal, it.principal)
+            assertEquals(handoverContext.assessmentContext, it.assessmentContext)
+            assertEquals(handoverContext.sentencePlanContext, it.sentencePlanContext)
+          },
+        )
+      }
+    }
+
+    @Test
+    fun `should save the handover token with the correct properties`() {
+      every { handoverContextService.saveContext(any()) } returns handoverContext
+      every { handoverTokenRepository.save(any()) } returns handoverToken
+
+      handoverService.createHandover(handoverRequest, handoverSessionId)
+
+      verify {
+        handoverTokenRepository.save(
+          withArg {
+            assertEquals(handoverSessionId, it.handoverSessionId)
+            assertEquals(handoverContext.principal, it.principal)
+            assertEquals(TokenStatus.UNUSED, it.tokenStatus)
+          },
+        )
+      }
+    }
+
+    @Test
+    fun `should return a valid handover link`() {
+      val domain = "handover-service"
+      val endpoints = TestUtils.createEndPoint()
+      every { handoverContextService.saveContext(any()) } returns handoverContext
+      every { handoverTokenRepository.save(any()) } returns handoverToken
+      every { appConfiguration.self.externalUrl } returns domain
+      every { appConfiguration.self.endpoints } returns endpoints
+
+      val result = handoverService.createHandover(handoverRequest, handoverSessionId)
+      assertContains(result.handoverLink, "${domain}${endpoints.handover}")
+      verify {
+        handoverTokenRepository.save(
+          withArg {
+            assertEquals(it.code, result.handoverLink.substringAfterLast('/'))
+          },
+        )
+      }
+    }
   }
 
-  @Test
-  fun `consume and exchange handover token should return authenticated token when valid token is used `() {
-    val handoverToken = TestUtils.createHandoverToken(TokenStatus.UNUSED)
+  @Nested
+  @DisplayName("consumeAndExchangeHandover")
+  inner class ConsumeAndExchangeHandover {
+    @Test
+    fun `should return authenticated token when valid token is used `() {
+      val handoverToken = TestUtils.createHandoverToken(TokenStatus.UNUSED)
+      val consumedToken = handoverToken.copy(
+        tokenStatus = TokenStatus.USED,
+      )
 
-    every { handoverTokenRepository.findById(any()) } returns Optional.of(handoverToken)
-    every { handoverTokenRepository.save(any()) } returns handoverToken
+      every { handoverTokenRepository.findById(any()) } returns Optional.of(handoverToken)
+      every { handoverTokenRepository.save(any()) } returns handoverToken
 
-    val result = handoverService.consumeAndExchangeHandover(handoverToken.code)
+      val result = handoverService.consumeAndExchangeHandover(handoverToken.code)
 
-    assertEquals(true, (result as UseHandoverLinkResult.Success).authenticationToken.isAuthenticated)
-    verify { handoverTokenRepository.findById(any()) }
-    verify { handoverTokenRepository.save(any()) }
-  }
+      assertEquals(true, (result as UseHandoverLinkResult.Success).authenticationToken.isAuthenticated)
+      verify { handoverTokenRepository.findById(any()) }
+      verify { handoverTokenRepository.save(handoverToken) }
+    }
 
-  @Test
-  fun `consume and exchange handover token should not save token and return HandoverLinkAlreadyUsed when used token is used `() {
-    val handoverToken = TestUtils.createHandoverToken(TokenStatus.USED)
+    @Test
+    fun `should not save token and return HandoverLinkAlreadyUsed when used token is used `() {
+      val handoverToken = TestUtils.createHandoverToken(TokenStatus.USED)
 
-    every { handoverTokenRepository.findById(any()) } returns Optional.of(handoverToken)
-    every { handoverTokenRepository.save(any()) } returns handoverToken
+      every { handoverTokenRepository.findById(any()) } returns Optional.of(handoverToken)
+      every { handoverTokenRepository.save(any()) } returns handoverToken
 
-    val result = handoverService.consumeAndExchangeHandover(handoverToken.code)
+      val result = handoverService.consumeAndExchangeHandover(handoverToken.code)
 
-    assertEquals(result, UseHandoverLinkResult.HandoverLinkAlreadyUsed)
-    verify { handoverTokenRepository.findById(any()) wasNot Called }
-    verify { handoverTokenRepository.save(any()) wasNot Called }
+      assertEquals(result, UseHandoverLinkResult.HandoverLinkAlreadyUsed)
+      verify { handoverTokenRepository.findById(any()) wasNot Called }
+      verify { handoverTokenRepository.save(any()) wasNot Called }
+    }
   }
 }
