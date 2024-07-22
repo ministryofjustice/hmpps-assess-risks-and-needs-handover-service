1030822c6463f0d023030b0f8244e44369aef31d
devkokov <dm.kokov@gmail.com>
2024-07-22 18:26:22 +0100

    Add more assertions to the test

--- e2e/cypress/e2e/spec.cy.js
+++ e2e/cypress/e2e/spec.cy.js
@@ -3,10 +3,13 @@ describe('End-to-end test', () => {
         cy.visit('/')
     })
     it('logs into SAN', () => {
-        cy.get('button').contains('Create handover link').click()
-        cy.get('a.govuk-button').contains('Open').invoke('attr', 'href').then((href) => {
-            cy.visit(href)
+        cy.get('#crn').invoke('val').then((crn) => {
+            cy.get('button').contains('Create handover link').click()
+            cy.get('a.govuk-button').contains('Open').invoke('attr', 'href').then((href) => {
+                cy.visit(href)
+            })
+            cy.get('.hmpps-header__title__service-name').should('contain.text', 'Strengths and needs')
+            cy.get('dd').should('contain.text', crn)
         })
-        cy.get('.hmpps-header__title__service-name').should('contain.text', 'Strengths and needs')
     })
 })
