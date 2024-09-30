1dac65153450c5fde8eec9b23b80b0b6857ffcc0
devkokov <dm.kokov@gmail.com>
2024-09-30 15:57:06 +0100

    ARN-2310 Fix linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/config/WebConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/config/WebConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.resource.WebJarsResourceResolver
 
 @Configuration
-class WebConfig: WebMvcConfigurer {
+class WebConfig : WebMvcConfigurer {
   override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
     registry
       .addResourceHandler("/webjars/**")
