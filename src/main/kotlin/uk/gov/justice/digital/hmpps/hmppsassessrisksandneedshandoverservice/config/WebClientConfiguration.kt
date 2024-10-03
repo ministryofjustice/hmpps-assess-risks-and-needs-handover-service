612c7c321d87beb3b518dde2e2649595607f7a1a
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-10-03 17:53:13 +0100

    Add WebClientConfiguration for ARNS Coordinator API

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/config/WebClientConfiguration.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.hmpps.kotlin.auth.authorisedWebClient
+import java.time.Duration
+
+@Configuration
+class WebClientConfiguration(
+  @Value("\${app.services.coordinator-api.base-url}") val coordinatorApiUri: String,
+  @Value("\${api.timeout:20s}") val timeout: Duration,
+) {
+  @Bean
+  fun coordinatorApiWebClient(authorizedClientManager: org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient =
+    builder.authorisedWebClient(authorizedClientManager, registrationId = "coordinator-api", url = coordinatorApiUri, timeout)
+}
