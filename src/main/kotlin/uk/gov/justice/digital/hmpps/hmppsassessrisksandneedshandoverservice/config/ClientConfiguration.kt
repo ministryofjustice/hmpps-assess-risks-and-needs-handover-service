f2f109a27311565b5a214ab7d0a1a5b6a4394bd9
Dimitar Kokov <dimitar.kokov@digital.justice.gov.uk>
2024-10-23 14:34:25 +0100

    Allow for optional clients (#73)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/config/ClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/config/ClientConfiguration.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.core.AuthorizationGrantType
@@ -9,6 +10,8 @@ import org.springframework.security.oauth2.server.authorization.client.InMemoryR
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings
+import java.net.URI
+import java.net.URISyntaxException
 import java.time.Duration
 
 @Configuration
@@ -23,25 +26,44 @@ class ClientConfiguration(
       .refreshTokenTimeToLive(Duration.ofDays(2L))
       .build()
 
-    val registeredClients = appConfiguration.clients.map { (clientId, clientProperties) ->
-      val clientBuilder = RegisteredClient.withId(clientId)
-        .clientId(clientId)
-        .clientSecret("{noop}${clientProperties.secret}")
-        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-        .scope(OidcScopes.OPENID)
-        .scope(OidcScopes.PROFILE)
-        .tokenSettings(tokenSettings)
-        .clientSettings(ClientSettings.builder().build())
+    val registeredClients = appConfiguration.clients
+      .filter { (clientId, clientProperties) ->
+        clientProperties.oauthRedirectUris.all { isUriValid(it) }
+          .also { clientIsValid ->
+            if (!clientIsValid) log.info("Client $clientId will be ignored due to a misconfigured oauth-redirect-uri.")
+          }
+      }
+      .map { (clientId, clientProperties) ->
+        val clientBuilder = RegisteredClient.withId(clientId)
+          .clientId(clientId)
+          .clientSecret("{noop}${clientProperties.secret}")
+          .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+          .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+          .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+          .scope(OidcScopes.OPENID)
+          .scope(OidcScopes.PROFILE)
+          .tokenSettings(tokenSettings)
+          .clientSettings(ClientSettings.builder().build())
 
-      clientProperties.oauthRedirectUris.forEach { uri ->
-        clientBuilder.redirectUri(uri)
+        clientProperties.oauthRedirectUris.forEach { uri ->
+          clientBuilder.redirectUri(uri)
+        }
+
+        clientBuilder.build()
       }
 
-      clientBuilder.build()
-    }
-
     return InMemoryRegisteredClientRepository(registeredClients)
+  }
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    private fun isUriValid(uri: String): Boolean {
+      return try {
+        URI(uri).fragment == null
+      } catch (_: URISyntaxException) {
+        false
+      }
+    }
   }
 }
