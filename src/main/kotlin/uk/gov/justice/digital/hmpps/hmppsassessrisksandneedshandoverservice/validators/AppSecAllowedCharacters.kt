f9124e9ece658f32ba3cf8ff46400cf1859af800
devkokov <dm.kokov@gmail.com>
2024-11-06 15:38:03 +0000

    SP2-900 AppSec Remediation - Field validation

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/validators/AppSecAllowedCharacters.kt
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.validators
+
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import jakarta.validation.ReportAsSingleViolation
+import jakarta.validation.constraints.Pattern
+import kotlin.reflect.KClass
+
+@Pattern(regexp = "^[a-zA-Z0-9-'\\s,./]+$")
+@Constraint(validatedBy = [])
+@ReportAsSingleViolation
+annotation class AppSecAllowedCharacters (
+  val message: String = "Field must contain only alphanumeric characters, hyphens, spaces, commas, full stops, forward slashes, or apostrophes",
+  val groups: Array<KClass<Any>> = [],
+  val payload: Array<KClass<Payload>> = [],
+)
