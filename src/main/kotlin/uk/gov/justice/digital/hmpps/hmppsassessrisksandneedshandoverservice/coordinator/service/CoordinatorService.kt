cb023fde06be4c2993fb40c5bdbe390475618e19
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-03-10 12:55:45 +0000

    Add retries to WebClientConfiguration

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/coordinator/service/CoordinatorService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/coordinator/service/CoordinatorService.kt
@@ -3,16 +3,12 @@ package uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.coo
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
-import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.config.AppConfiguration
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.coordinator.response.AssociationsResponse
 
 @Service
 class CoordinatorService(
-  val appConfiguration: AppConfiguration,
-  val coordinatorApiWebClient: WebClient,
+  private val coordinatorApiWebClient: WebClient,
 ) {
-  private fun endpoint(endpoint: String): String = "${appConfiguration.services.coordinatorApi.baseUrl}$endpoint"
-
   fun getAssociations(oasysAssessmentPk: String, planVersion: Long?): AssociationsResponse = try {
     val result = coordinatorApiWebClient.get()
       .uri { uriBuilder ->
@@ -27,8 +23,8 @@ class CoordinatorService(
     result
       ?: throw IllegalStateException("Unexpected empty associations response for OASys Assessment PK $oasysAssessmentPk")
   } catch (ex: WebClientResponseException) {
-    throw Exception("Unexpected associations response code ${ex.statusCode}")
+    throw Exception("Unexpected associations response code ${ex.statusCode}", ex)
   } catch (ex: Exception) {
-    throw Exception("Unexpected associations exception: ${ex.message}")
+    throw Exception("Unexpected associations exception: ${ex.message}", ex)
   }
 }
