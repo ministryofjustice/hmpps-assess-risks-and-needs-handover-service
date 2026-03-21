2f26d28f7697ce82f37cf562837acc9ba8a99a27
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-03-21 00:07:53 +0000

    Disable a bunch of unused OIDC/OAuth2 flows

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/authorization/entity/JpaAuthorization.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/authorization/entity/JpaAuthorization.kt
@@ -5,11 +5,7 @@ import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
 import org.springframework.data.redis.core.index.Indexed
 import org.springframework.security.oauth2.core.OAuth2AccessToken
-import org.springframework.security.oauth2.core.OAuth2DeviceCode
-import org.springframework.security.oauth2.core.OAuth2RefreshToken
-import org.springframework.security.oauth2.core.OAuth2UserCode
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames
-import org.springframework.security.oauth2.core.oidc.OidcIdToken
 import org.springframework.security.oauth2.server.authorization.OAuth2Authorization
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationCode
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.authorization.service.JpaOAuth2AuthorizationService
@@ -25,6 +21,7 @@ data class JpaAuthorization(
   var principalName: String? = null,
   var authorizationGrantType: String? = null,
   var authorizedScopes: String? = null,
+  var authorizationCodeContext: String? = null,
   var attributes: String? = null,
   @Indexed var state: String? = null,
 
@@ -40,27 +37,6 @@ data class JpaAuthorization(
   var accessTokenType: String? = null,
   var accessTokenScopes: String? = null,
 
-  @Indexed var refreshTokenValue: String? = null,
-  var refreshTokenIssuedAt: Instant? = null,
-  var refreshTokenExpiresAt: Instant? = null,
-  var refreshTokenMetadata: String? = null,
-
-  @Indexed var oidcIdTokenValue: String? = null,
-  var oidcIdTokenIssuedAt: Instant? = null,
-  var oidcIdTokenExpiresAt: Instant? = null,
-  var oidcIdTokenMetadata: String? = null,
-  var oidcIdTokenClaims: String? = null,
-
-  @Indexed var userCodeValue: String? = null,
-  var userCodeIssuedAt: Instant? = null,
-  var userCodeExpiresAt: Instant? = null,
-  var userCodeMetadata: String? = null,
-
-  @Indexed var deviceCodeValue: String? = null,
-  var deviceCodeIssuedAt: Instant? = null,
-  var deviceCodeExpiresAt: Instant? = null,
-  var deviceCodeMetadata: String? = null,
-
   @TimeToLive(unit = TimeUnit.MINUTES) var timeout: Long = 30L,
 ) : Serializable {
   init {
@@ -72,10 +48,6 @@ data class JpaAuthorization(
     val expiresAtList = listOfNotNull(
       this.authorizationCodeExpiresAt,
       this.accessTokenExpiresAt,
-      this.refreshTokenExpiresAt,
-      this.oidcIdTokenExpiresAt,
-      this.userCodeExpiresAt,
-      this.deviceCodeExpiresAt,
     )
 
     return expiresAtList.maxOfOrNull { Duration.between(Instant.now(), it).toMinutes() }
@@ -89,41 +61,12 @@ data class JpaAuthorization(
     authorizationCodeMetadata = JpaOAuth2AuthorizationService.writeMap(auth.metadata)
   }
 
-  private fun mapOidcIdToken(auth: OAuth2Authorization.Token<OidcIdToken>) {
-    oidcIdTokenValue = auth.token.tokenValue
-    oidcIdTokenIssuedAt = auth.token.issuedAt
-    oidcIdTokenExpiresAt = auth.token.expiresAt
-    oidcIdTokenMetadata = JpaOAuth2AuthorizationService.writeMap(auth.metadata)
-    oidcIdTokenClaims = auth.claims?.run(JpaOAuth2AuthorizationService::writeMap)
-  }
-
   private fun mapAccessToken(auth: OAuth2Authorization.Token<OAuth2AccessToken>) {
     accessTokenValue = auth.token.tokenValue
     accessTokenIssuedAt = auth.token.issuedAt
     accessTokenExpiresAt = auth.token.expiresAt
     accessTokenMetadata = JpaOAuth2AuthorizationService.writeMap(auth.metadata)
     accessTokenScopes = auth.token.scopes?.joinToString(",")
-  }
-
-  private fun mapRefreshToken(auth: OAuth2Authorization.Token<OAuth2RefreshToken>) {
-    refreshTokenValue = auth.token.tokenValue
-    refreshTokenIssuedAt = auth.token.issuedAt
-    refreshTokenExpiresAt = auth.token.expiresAt
-    refreshTokenMetadata = JpaOAuth2AuthorizationService.writeMap(auth.metadata)
-  }
-
-  private fun mapUserCode(auth: OAuth2Authorization.Token<OAuth2UserCode>) {
-    userCodeValue = auth.token.tokenValue
-    userCodeIssuedAt = auth.token.issuedAt
-    userCodeExpiresAt = auth.token.expiresAt
-    userCodeMetadata = JpaOAuth2AuthorizationService.writeMap(auth.metadata)
-  }
-
-  private fun mapDeviceCode(auth: OAuth2Authorization.Token<OAuth2DeviceCode>) {
-    deviceCodeValue = auth.token.tokenValue
-    deviceCodeIssuedAt = auth.token.issuedAt
-    deviceCodeExpiresAt = auth.token.expiresAt
-    deviceCodeMetadata = JpaOAuth2AuthorizationService.writeMap(auth.metadata)
   }
 
   companion object {
@@ -135,15 +78,15 @@ data class JpaAuthorization(
       entity.principalName = authorization.principalName
       entity.authorizationGrantType = authorization.authorizationGrantType.value
       entity.authorizedScopes = authorization.authorizedScopes.joinToString(",")
-      entity.attributes = JpaOAuth2AuthorizationService.writeMap(authorization.attributes)
+      AuthorizationCodeContext.from(authorization)?.let {
+        entity.authorizationCodeContext = JpaOAuth2AuthorizationService.writeAuthorizationCodeContext(it)
+      } ?: run {
+        entity.attributes = JpaOAuth2AuthorizationService.writeMap(authorization.attributes)
+      }
       entity.state = authorization.getAttribute(OAuth2ParameterNames.STATE)
 
       authorization.getToken(OAuth2AuthorizationCode::class.java)?.let(entity::mapOAuth2AuthorizationCode)
-      authorization.getToken(OidcIdToken::class.java)?.let(entity::mapOidcIdToken)
       authorization.getToken(OAuth2AccessToken::class.java)?.let(entity::mapAccessToken)
-      authorization.getToken(OAuth2RefreshToken::class.java)?.let(entity::mapRefreshToken)
-      authorization.getToken(OAuth2UserCode::class.java)?.let(entity::mapUserCode)
-      authorization.getToken(OAuth2DeviceCode::class.java)?.let(entity::mapDeviceCode)
 
       return entity
     }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/authorization/repository/JpaAuthorizationRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsassessrisksandneedshandoverservice/authorization/repository/JpaAuthorizationRepository.kt
@@ -4,18 +4,9 @@ import org.springframework.data.repository.CrudRepository
 import uk.gov.justice.digital.hmpps.hmppsassessrisksandneedshandoverservice.authorization.entity.JpaAuthorization
 
 interface JpaAuthorizationRepository : CrudRepository<JpaAuthorization, String> {
-
   fun findByAuthorizationCodeValue(token: String): JpaAuthorization?
 
   fun findByAccessTokenValue(token: String): JpaAuthorization?
-
-  fun findByRefreshTokenValue(token: String): JpaAuthorization?
-
-  fun findByOidcIdTokenValue(token: String): JpaAuthorization?
-
-  fun findByUserCodeValue(token: String): JpaAuthorization?
-
-  fun findByDeviceCodeValue(token: String): JpaAuthorization?
 
   fun findByState(token: String): JpaAuthorization?
 }
